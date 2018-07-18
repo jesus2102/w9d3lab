28fbfd0a51210dd5fa676fc2a1259fd11db162ad
Adams1991 <adams121@hotmail.co.uk>
2018-07-18 12:45:32 +0100

    DB Helper setup and pom setup

--- src/main/java/db/DBHelper.java
+++ src/main/java/db/DBHelper.java
@@ -1,4 +1,93 @@
 package db;
 
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
+
+import java.util.List;
+
 public class DBHelper {
+    private static Transaction transaction;
+    private static Session session;
+
+    // TODO: SAVE METHOD
+    public static void save(Object object){
+        session = HibernateUtil.getSessionFactory().openSession();
+        try {
+            transaction = session.beginTransaction();
+            session.save(object);
+            transaction.commit();
+        } catch (HibernateException e) {
+            transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+
+
+    }
+
+    //TODO: UPDATE METHOD
+    public static void update(Object object){
+        session = HibernateUtil.getSessionFactory().openSession();
+        try {
+            transaction = session.beginTransaction();
+            session.update(object);
+            transaction.commit();
+        } catch (HibernateException e) {
+            transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+
+    //TODO: DELETE METHOD
+    public static void delete(Object object){
+        session = HibernateUtil.getSessionFactory().openSession();
+        try {
+            transaction = session.beginTransaction();
+            session.delete(object);
+            transaction.commit();
+        } catch (HibernateException e) {
+            transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+
+    // TODO: FINDALL METHOD
+    public static <T> List<T> getAll(Class classType){
+        List<T> results = null;
+        session = HibernateUtil.getSessionFactory().openSession();
+        try{
+
+            Criteria cr = session.createCriteria(classType);
+            results = cr.list();
+        } catch (HibernateException e){
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return results;
+    }
+
+    // TODO: FIND BY ID
+    public static <T> T findById(int id, Class classType) {
+        T result = null;
+        session = HibernateUtil.getSessionFactory().openSession();
+        try {
+            Criteria cr = session.createCriteria(classType);
+            cr.add(Restrictions.eq("id", id));
+            result = (T)cr.uniqueResult();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return result;
+    }
 }
