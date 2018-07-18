3cdcc5f915316e31201c9043705337d5d343dafa
jesus2102 <jesusperezmarin@gmail.com>
2018-07-18 13:36:41 +0100

    All Test Borrower passed

--- src/test/java/BorrowerTest.java
+++ src/test/java/BorrowerTest.java
@@ -1,6 +1,27 @@
 import models.Borrower;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class BorrowerTest {
 
-    Borrower borrower
+    Borrower borrower;
+
+    @Before
+    public void before(){
+        borrower = new Borrower("Shaun");
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Shaun", borrower.getName());
+    }
+
+    @Test
+    public void canSetName(){
+        borrower.setName("Jesus");
+        assertEquals("Jesus", borrower.getName());
+    }
+
 }
