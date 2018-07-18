9d93181fef14eb8a1e813e9d8bac83fd646cf5ed
jesus2102 <jesusperezmarin@gmail.com>
2018-07-18 14:01:49 +0100

    half test book done

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -1,2 +1,12 @@
+import models.Book;
+import org.junit.Before;
+
 public class BookTest {
+
+    Book book;
+
+    @Before
+    public void before(){
+        book = new Book("Kama-sutra", "Tomas Turbado")
+    }
 }
